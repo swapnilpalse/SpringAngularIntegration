ad9e04b8c15743f2d17d5c6d741712e1f1d4890b
swapnil palse <swapnil.palse@gmail.com>
2018-05-03 19:52:51 -0700

    Added the frot end

--- src/main/webapp/angular/src/app/app.component.ts
+++ src/main/webapp/angular/src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+  '../../node_modules/bootstrap/dist/css/bootstrap.css']
 })
 export class AppComponent {
   title = 'app';
+  public intro_attractions =require("./images/intro_attractions.jpg");
+  public intro_room =require("./images/intro_room.jpg");
+    public intro_pool =require("./images/intro_pool.jpg");
+    public intro_dining =require("./images/intro_dining.jpg");
+    public intro_wedding =require("./images/intro_wedding.jpg");
+      public arrow =require("./images/arrow.png");
+      public dining_rooftop =require("./images/dining_rooftop.jpg");
+public dining_smoothiebar =require("./images/dining_smoothiebar.jpg");
+public dining_lattes =require("./images/dining_lattes.jpg");
+    public twitter =require("./images/twitter.png");  
+      public facebook =require("./images/facebook.png");     
+      public youtube =require("./images/youtube.png");    
+      
+    
 }
